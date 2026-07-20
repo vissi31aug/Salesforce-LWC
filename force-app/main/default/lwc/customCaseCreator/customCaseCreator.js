70cad7cdaec729fffb37721c180d1b37af3fee20
VishalKashyap0831 <vishal.k@justbaat.com>
2026-07-20 09:49:07 +0530

    Get record info added

--- force-app/main/default/lwc/customCaseCreator/customCaseCreator.js
+++ force-app/main/default/lwc/customCaseCreator/customCaseCreator.js
@@ -1,16 +1,34 @@
 
-import { LightningElement } from 'lwc';
+import { LightningElement, wire } from 'lwc';
 import { createRecord } from 'lightning/uiRecordApi';
 import CASE_OBJ from '@salesforce/schema/Case';
 import CASE_SUBJECT from '@salesforce/schema/Case.Subject';
 import CASE_PRIORITY from '@salesforce/schema/Case.Priority';
-import CASE_DISCRIPTION from '@salesforce/schema/Case.Description'
+import CASE_DISCRIPTION from '@salesforce/schema/Case.Description';
+import {getObjectInfo} from 'lightning/uiObjectInfoApi';
+import CASE_RECORD_TYPE from '@salesforce/schema/Case.RecordTypeId';
+
 
 export default class CustomCaseCreator extends LightningElement {
 
 subject='';
 priority='';
 discription='';
+recordTypeId='';
+
+@wire(getObjectInfo,{objectApiName:CASE_OBJ}) caseRecord({data,error}){
+
+    if(data){
+        let recordTypeDetails=data.recordTypeInfos;
+        Object.keys(recordTypeDetails).forEach((element => {
+           const recordTypeInfo=recordTypeDetails[element];
+            if(recordTypeInfo.name=='Case Info Team'){
+               this.recordTypeId=recordTypeInfo.recordTypeId;
+            }
+            
+        }))
+    }
+}
 
 get options() {
         return [
@@ -42,6 +60,7 @@ async handleButton(){
     fields[CASE_SUBJECT.fieldApiName]=this.subject;
     fields[CASE_PRIORITY.fieldApiName]=this.priority;
     fields[CASE_DISCRIPTION.fieldApiName]=this.discription;
+    fields[CASE_RECORD_TYPE.fieldApiName]=this.recordTypeId;
     
     let recordInput={apiName:CASE_OBJ.objectApiName, fields};
     await createRecord(recordInput)
