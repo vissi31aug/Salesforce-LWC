cd4ba5fe31dcbfe3881482c84ec74c7b3840faa6
VishalKashyap0831 <vishal.k@justbaat.com>
2026-07-23 18:51:00 +0530

    Fetch Dynamic Related Record

--- force-app/main/default/lwc/dynamicRelatedRecord/dynamicRelatedRecord.js
+++ force-app/main/default/lwc/dynamicRelatedRecord/dynamicRelatedRecord.js
@@ -1,3 +1,47 @@
-import { LightningElement } from 'lwc';
+import { LightningElement, api, track } from 'lwc';
+import fetchRecord from '@salesforce/apex/ChildRecordController.fetchRecord';
 
-export default class DynamicRelatedRecord extends LightningElement {}
+export default class DynamicRelatedRecord extends LightningElement {
+
+
+    @api recordId;
+    error = '';
+    @track data = [];
+
+    @api childObjectApiName= 'Opportunity';
+    @api fieldList= 'Id, Name, StageName, CloseDate';
+    @api lookupFieldApiName= 'AccountId';
+    @api limitSize = 5;
+
+    connectedCallback() {
+        this.loadChildRecord();
+    }
+
+    async loadChildRecord() {
+        try {
+            const result = await fetchRecord({
+                recordId: this.recordId,
+                childObjectApiName: this.childObjectApiName,
+                fieldList: this.fieldList,
+                lookupFieldApiName: this.lookupFieldApiName,
+                limitSize: this.limitSize
+            });
+
+            console.log('reached here' + JSON.stringify(result));
+            this.data = result;
+
+        }
+        catch (e) {
+            console.log('reached here with error');
+            this.error = e.body.message;
+
+        }
+    }
+
+    get columns(){
+
+        const fields=this.fieldList.split(',').map(f=>f.trim()).filter(Boolean);
+        return fields.map(field => ({label:field, fieldName:field}));
+    }
+
+}
